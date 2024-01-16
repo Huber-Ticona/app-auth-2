6197cc1623015d815a3743f99d4d58c0598d7983
HuberTicona <super__xd@hotmail.com>
2024-01-16 16:43:05 -0300

    add /docs ,add base path /<name_repo>

---
+++ docs/_app/immutable/entry/start.5IBvftzD.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.G6XJ5YL3.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
-
+console.log("CREANDO APP");
+console.log("env basepath: ", process.env.BASE_PATH);
 export default {
     kit: {
         adapter: adapter({
-            fallback: "200.html", // may differ from host to host
+            fallback: "index.html",
         }),
+        paths: {
+            base: "/app-auth-2",
+        },
     },
 };
